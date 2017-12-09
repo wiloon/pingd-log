487431d4761ed5011068100cda9fee11262f7497
wiloon <wiloon.wy@gmail.com>
2017-12-09 12:24:35 +0800

    init

---
+++ log/config.go
@@ -0,0 +1,44 @@
+package log
+
+import (
+	"strings"
+	"path/filepath"
+	"github.com/sirupsen/logrus"
+)
+
+type Conf struct {
+	Level       string
+	To          string
+	LogFilePath string
+	ToConsole   bool
+	ProjectName string
+}
+
+func (config *Conf) GetParentPath() string {
+	return "/data/logs/" + config.ProjectName
+}
+
+func (config *Conf) GetFullPath() string {
+	return config.GetParentPath() + string(filepath.Separator) + config.Level + ".log"
+}
+
+func (config *Conf) Init() *Conf {
+	if strings.Contains(config.To, "console") {
+		config.ToConsole = true
+	}
+	return config
+}
+
+func (config *Conf) GetLogrusLevel() logrus.Level {
+	var level logrus.Level
+	if strings.EqualFold(config.Level, "debug") {
+		level = logrus.DebugLevel
+	}
+	if strings.EqualFold(config.Level, "info") {
+		level = logrus.InfoLevel
+	}
+	if strings.EqualFold(config.Level, "warn") {
+		level = logrus.WarnLevel
+	}
+	return level
+}
---
+++ log/log.go
@@ -0,0 +1,66 @@
+package log
+
+import (
+	_ "golang.org/x/sys/unix"
+	"os"
+	"log"
+	"github.com/sirupsen/logrus"
+	"github.com/wiloon/app-config"
+)
+
+var fileLogger *logrus.Logger
+var stdLogger *logrus.Logger
+
+var file *os.File
+var logConfig *Conf
+
+func init() {
+	logConfig = (&Conf{
+		Level:       config.GetString("log.level", "debug"),
+		To:          config.GetString("log.to", "file"),
+		ProjectName: config.GetString(config.KeyProjectName, ""),
+	}).Init()
+
+	fileLogger = getFileLogger()
+	stdLogger = getStdLogger()
+}
+
+func Info(args ...interface{}) {
+	fileLogger.Info(args...)
+
+	if logConfig.ToConsole {
+		stdLogger.Info(args...)
+	}
+}
+
+func getStdLogger() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetLevel(logConfig.GetLogrusLevel())
+	logger.Out = os.Stdout
+	logger.Formatter = &logrus.TextFormatter{}
+
+	logger.Info("logger init done, logrus")
+	return logger
+}
+
+func getFileLogger() *logrus.Logger {
+	logger := logrus.New()
+
+	logger.SetLevel(logConfig.GetLogrusLevel())
+
+	os.MkdirAll(logConfig.GetParentPath(), 0744)
+	logFile := logConfig.GetFullPath()
+
+	var err error
+	file, err = os.OpenFile(logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err == nil {
+		log.SetOutput(file)
+	} else {
+		logrus.Info("Failed to log to file, using default stderr,error:", err)
+	}
+	logger.Out = file
+	logger.Formatter = &logrus.TextFormatter{}
+
+	logger.Info("logger init done, logrus")
+	return logger
+}
